6051d4284384c137d4fcbd9967a30f8151c5e3df
Qi <1342592931@qq.com>
2021-07-01 22:53:04 +0800

    完成商品功能

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import router from './router'
 import './plugins/element.js'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
 
 import './assets/css/global.css'
+
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
@@ -19,7 +25,26 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+// 注册第三方组件
 Vue.component('tree-table', TreeTable)
+
+// 注册富文本编辑器为全局组件
+Vue.use(VueQuillEditor)
+
+// 注册全局过滤器
+Vue.filter('dateFormat', originVal => {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
